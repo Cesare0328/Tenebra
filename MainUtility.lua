1984579fa651178ddc309b7be16acc515a52120a
Cesare <79593536+Cesare0328@users.noreply.github.com>
2024-03-14 07:19:59 -0700

    Update MainUtility.lua

--- MainUtility.lua
+++ MainUtility.lua
@@ -3674,8 +3674,17 @@ local ClosureBindings = {
 				end
 
 				UserInputService.InputBegan:Connect(function(input)
-					if input.KeyCode == Enum.KeyCode.Tab and Textbox.Input:IsFocused() and prediction ~= "" then
-						Input:SetValue(prediction, true)
+					if input.KeyCode == Enum.KeyCode.Tab then
+						if prediction ~= "" then	
+
+                                                        task.wait()
+							Input.Value = prediction
+							Box.Text = prediction
+							prediction = ""
+
+							Library:SafeCallback(Input.Callback, Input.Value)
+							Library:SafeCallback(Input.Changed, Input.Value)
+						end
 					end
 				end)
 				task.spawn(function()
