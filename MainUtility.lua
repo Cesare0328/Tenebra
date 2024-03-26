f846589accc8a1d23bb895e5a1bd72e6faa5f0df
Cesare <79593536+Cesare0328@users.noreply.github.com>
2024-03-26 08:26:03 -0700

    Update MainUtility.lua

--- MainUtility.lua
+++ MainUtility.lua
@@ -1787,8 +1787,8 @@ local ClosureBindings = {
 				})
 
 				Window.ContainerCanvas = New("Frame", {
-					Size = UDim2.new(1, -Window.TabWidth - 32, 1, -102),
-					Position = UDim2.fromOffset(Window.TabWidth + 26, 90),
+					Size = UDim2.new(1, -Window.TabWidth - 32, 1, -72),
+					Position = UDim2.fromOffset(Window.TabWidth + 26, 60),
 					BackgroundTransparency = 1,
 				}, {
 					Window.ContainerAnim,
