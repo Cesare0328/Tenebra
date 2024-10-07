855e28fd0bcece373c572376bbf7408a2aef4ec5
Cesare <79593536+Cesare0328@users.noreply.github.com>
2024-10-07 08:24:11 -0700

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,9 +1,9 @@
 repeat task.wait() until game:IsLoaded()
 if getgenv().TenebraLoadedX then return end
-if game.PlaceId == 12604352060 then
+if game.PlaceId == 12604352060 or game.PlaceId == 18122780138 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6214e32f2450f4f49fcebfa21e4839d0.lua"))()
 elseif game.PlaceId == 3272915504 then 
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a766b6ca36696855978a701ca1fa601d.lua"))()
-elseif game.PlaceId == 15449776494 then
+elseif game.PlaceId == 15449776494 or game.PlaceId == 18122795128 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6a4881579b97c854344bc961ba379807.lua"))()
 end
